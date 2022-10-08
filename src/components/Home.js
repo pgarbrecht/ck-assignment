f7d0f062d3ec1278f3880187f094c9497f10905d
pgarbrecht <philgarbrecht@gmail.com>
2022-10-08 13:05:11 -0500

    home component connected to app

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import '../App.css';
+import React, {Component} from 'react'
+
+
+class Home extends Component {
+	constructor(props) {
+		super(props)
+    }
+	render() {
+		return (
+			<p>Home component</p>
+		)
+	}
+}
+
+export default Home
